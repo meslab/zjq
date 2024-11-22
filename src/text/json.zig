cc2ba6c64c4143b5e1c201f532d694bfd71dc53c
Anton Sidorov <antosid@gmail.com>
2024-11-22 18:18:07 +0000

    tests: -

--- src/text/json.zig
+++ src/text/json.zig
@@ -37,11 +37,20 @@ pub const T = struct {
     }
 
     pub fn get(self: T, query: []const u8) T {
-        if (self.t.?.object.get(query) == null) {
-            std.debug.print("No such value: {s}\n", .{query});
+        const delimiter = ".";
+        var split_query = std.mem.split(u8, query, delimiter);
+
+        var current = self.t.?;
+        while (split_query.next()) |part| {
+            // std.debug.print("Query: {s}\n", .{part});
+            if (current.object.get(part) == null) {
+                std.debug.print("No such value: {s}\n", .{part});
+                break;
+            }
+            current = current.object.get(part).?;
         }
 
-        return T.init(self.t.?.object.get(query).?);
+        return T.init(current);
     }
 };
 
@@ -68,10 +77,62 @@ test "json unpack" {
 
     const result = json.get("test");
 
-    //try testing.expectEqualStrings("test", result.get("."));
-
     const test_string = try std.json.stringifyAlloc(allocator, result, .{});
     defer allocator.free(test_string);
 
     try testing.expectEqualStrings("{\"t\":\"test\"}", test_string);
+}
+
+test "json unpack 2" {
+    const allocator = std.testing.allocator;
+
+    const test_json_string =
+        \\ {"test": "test",
+        \\ "zest": ["z","e"],
+        \\ "fest": null,
+        \\ "isit": true,
+        \\ "ns": "1232",
+        \\ "in": 12343,
+        \\ "a": {"a":"2", "b": 123, "c": true, "d": null}
+        \\}
+    ;
+
+    const parsed_json = try std.json.parseFromSlice(std.json.Value, std.testing.allocator, test_json_string, .{});
+    defer parsed_json.deinit();
+
+    const value = parsed_json.value;
+
+    const json = T.init(value);
+
+    const result = json.get("a").get("a");
+    const test_string = try std.json.stringifyAlloc(allocator, result, .{});
+    defer allocator.free(test_string);
+    try testing.expectEqualStrings("{\"t\":\"2\"}", test_string);
+}
+
+test "json unpack 3" {
+    const allocator = std.testing.allocator;
+
+    const test_json_string =
+        \\ {"test": "test",
+        \\ "zest": ["z","e"],
+        \\ "fest": null,
+        \\ "isit": true,
+        \\ "ns": "1232",
+        \\ "in": 12343,
+        \\ "a": {"a":"2", "b": 123, "c": true, "d": null}
+        \\}
+    ;
+
+    const parsed_json = try std.json.parseFromSlice(std.json.Value, std.testing.allocator, test_json_string, .{});
+    defer parsed_json.deinit();
+
+    const value = parsed_json.value;
+
+    const json = T.init(value);
+
+    const result = json.get("a.a");
+    const test_string = try std.json.stringifyAlloc(allocator, result, .{});
+    defer allocator.free(test_string);
+    try testing.expectEqualStrings("{\"t\":\"2\"}", test_string);
 }
