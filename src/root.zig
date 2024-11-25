e751d77579c0c60b1a60691dbe4c201fcb3b67ee
Anton Sidorov <antosid@gmail.com>
2024-11-25 22:48:56 +0000

    cleanup:

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,4 @@
-pub const add = @import("./math/addition.zig");
-pub const multi = @import("./math/milti.zig");
-pub const str = @import("./text/json.zig");
+pub const json = @import("./text/json.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
